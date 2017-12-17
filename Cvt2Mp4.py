5e5b62779537c14e456888774dbc4058b1eaa49a
Andrew Byron <avgbyron@email.arizona.edu>
2017-12-17 09:32:41 -0700

    Changing FileHelper.cvt2mp4 to return a string with new mp4 file

--- Cvt2Mp4.py
+++ Cvt2Mp4.py
@@ -19,6 +19,7 @@ class FileHelper:
     with open(time.strftime("%Y%m%d_%H%M%S") + ".mp4", 'w') as outFile:
       print(outFile.name)
       call("MP4Box -add " + "./temp.h264" + outFile.name , shell = True)
+      return outFile.name
 
 
 
